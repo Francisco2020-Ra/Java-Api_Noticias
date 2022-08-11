c162edde553647c75830afbb822a70957352ae8b
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-08-10 22:48:09 -0300

    Adding endpoint createArticle

---
+++ src/main/java/com/javainformatorio/apinoticias/controller/ArticleController.java
@@ -0,0 +1,28 @@
+package com.javainformatorio.apinoticias.controller;
+
+import com.javainformatorio.apinoticias.dto.ArticleDTO;
+import com.javainformatorio.apinoticias.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/article")
+public class ArticleController {
+
+    private ArticleService articleService;
+
+    @Autowired
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createArticle(@RequestBody ArticleDTO articleDTO) {
+        return new ResponseEntity<>(articleService.createArticle(articleDTO), HttpStatus.CREATED);
+    }
+}
--- src/main/java/com/javainformatorio/apinoticias/mapper/ArticleMapper.java
+++ src/main/java/com/javainformatorio/apinoticias/mapper/ArticleMapper.java
@@ -1,7 +1,12 @@
 package com.javainformatorio.apinoticias.mapper;
 
 import com.javainformatorio.apinoticias.dto.ArticleDTO;
+import com.javainformatorio.apinoticias.dto.AuthorDTO;
+import com.javainformatorio.apinoticias.dto.SourceDTO;
 import com.javainformatorio.apinoticias.entities.ArticleEntity;
+import com.javainformatorio.apinoticias.entities.AuthorEntity;
+import com.javainformatorio.apinoticias.entities.SourceEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -9,6 +14,13 @@ import java.time.LocalDate;
 @Component
 public class ArticleMapper {
 
+    private final SourceMapper sourceMapper;
+    private final AuthorMapper authorMapper;
+    @Autowired
+    public ArticleMapper(SourceMapper sourceMapper, AuthorMapper authorMapper){
+        this.sourceMapper = sourceMapper;
+        this.authorMapper = authorMapper;
+    }
     public ArticleEntity toEntity(ArticleDTO articleDTO){
         return ArticleEntity.builder()
                 .title(articleDTO.getTitle())
@@ -17,7 +29,17 @@ public class ArticleMapper {
                 .urlToImage(articleDTO.getUrlToImage())
                 .publishedAt(LocalDate.parse(articleDTO.getPublishedAt()))
                 .content(articleDTO.getContent())
+                .source(getSourceEntity(articleDTO))
+                .author(getAuthorEntity(articleDTO))
                 .build();
+    }
+
+    public SourceEntity getSourceEntity(ArticleDTO articleDTO){
+        return sourceMapper.toEntity(articleDTO.getSource());
+    }
+
+    public AuthorEntity getAuthorEntity(ArticleDTO articleDTO){
+        return authorMapper.toEntity(articleDTO.getAuthor());
     }
 
     public ArticleDTO toDTO(ArticleEntity articleEntity){
@@ -29,6 +51,16 @@ public class ArticleMapper {
                 .urlToImage(articleEntity.getUrlToImage())
                 .publishedAt(articleEntity.getPublishedAt().toString())
                 .content(articleEntity.getContent())
+                .source(getSourceDTO(articleEntity))
+                .author(getAuthorDTO(articleEntity))
                 .build();
+    }
+
+    public SourceDTO getSourceDTO(ArticleEntity articleEntity){
+        return sourceMapper.toDTO(articleEntity.getSource());
+    }
+
+    public AuthorDTO getAuthorDTO(ArticleEntity articleEntity){
+        return authorMapper.toDTO(articleEntity.getAuthor());
     }
 }
